759bfc1441afbd0a73d3abebeeb79403cc21e0d6
Shrine Sidgel <123295702+ShrineSigdel@users.noreply.github.com>
2023-03-14 13:51:09 +0545

    Added UI to the game.

--- game.js
+++ game.js
@@ -1,52 +1,49 @@
-// Project: TOP - ROCK PAPER AND SCISSORS
+//PROJECT: ROCK, PAPER & SCISSORS 
 
-//main
-let result, playerSelection, computerSelection;
-let playerScore =0, computerScore=0;
-for (let i = 1; i <= 5; i++) {
+const buttons = document.querySelectorAll('button');
+let audio_winner = new Audio('./audio/winner.mp3');
+let computerSelection, playerSelection, playerScore = 0, computerScore = 0;
+let myScore = document.querySelector('.my_score');
+let compScore = document.querySelector('.computer_score');
+let result = document.querySelector('.result');
 
-    getInput();
-    result = gamePlay(playerSelection, computerSelection);
-    switch (result) {
-        case "tied":
-            console.log("\nTied!!!");
-            break;
-        case "won":
-            console.log("\nYou won as " + playerSelection + " beats " + computerSelection);
-            break;
-        case "lost":
-            console.log("\nYou lost as " + playerSelection + " loses to " + computerSelection);
-            break;
-    }
-}
-getScore();
+buttons.forEach(button => button.addEventListener('click', (item) => {
+    playerSelection = item.currentTarget.className;
+    computerSelection = getComputerChoice();
+    console.log(`${playerSelection} ${computerSelection}`);
+    let score = gamePlay(playerSelection, computerSelection);
+    getScore(score);
 
-
-//Entire game functions are here.
-//displaying the score
-function getScore() {
-
-    if(playerScore > computerScore) {
-        console.log("\n\nBooyaka Wowza! You defeated the computer.");
-    }
-    else if (playerScore == computerScore){
-        console.log("\n\nWow, That was a tough fought battle but none's worthy.");
+}));
+//gamePlay
+function gamePlay(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "tied";
     }
     else {
-        console.log("\n\nOh baby baby baby! You lost.");
+        switch (playerSelection) {
+            case "rock":
+                if (computerSelection == "paper") {
+                    return "lost";
+                } else {
+                    return "won";
+                }
+            case "paper":
+                if (computerSelection == "scissors") {
+                    return "lost";
+                } else {
+                    return "won";
+                }
+            case "scissors":
+                if (computerSelection == "rock") {
+                    return "lost";
+                } else {
+                    return "won";
+                }
+        }
     }
 }
-
-
-//taking the input
-function getInput() {
-    
-    computerSelection = getComputerChoice();
-    playerSelection = prompt("Choose rock, paper or scissors");
-    playerSelection = playerSelection.toLowerCase();
-}
-
-//throw a random choice.
+// //throw a random choice by computer.
 function getComputerChoice() {
     let check = parseInt(Math.random() * 3 + 1);
     if (check == 1) {
@@ -59,46 +56,34 @@ function getComputerChoice() {
         return "scissors";
     }
 }
-
-//round of rock_paper_scissors
-function gamePlay(playerSelection, computerSelection) {
-    //player == computer tied
-    if (playerSelection == computerSelection) {
+function getScore(score) {
+    if (score == "won") {
+        result.textContent = `Won!!! as ${playerSelection} beats ${computerSelection}`;
         playerScore++;
-        computerScore++;
-        return "tied";
     }
-    //else
+    else if (score == "lost") {
+        result.textContent = `Lost!!! as computer chose ${computerSelection}`;
+        computerScore++;
+    }
     else {
-        switch (playerSelection) {
-            case "rock":
-                if (computerSelection == "paper") {
-                    computerScore++;
-                    return "lost";
-                } else {
-                    playerScore++;
-                    return "won";
-                }
-
-            case "paper":
-                if (computerSelection == "scissors") {
-                    computerScore++;
-                    return "lost";
-                } else {
-                    playerScore++;
-                    return "won";
-                }
-
-            case "scissors":
-                if (computerSelection == "rock") {
-                    computerScore++;
-                    return "lost";
-                } else {
-                    playerScore++;
-                    return "won";
-                }
+        result.textContent = `Tied as both chose ${playerSelection}`;
+    }
+    compScore.textContent = computerScore;
+    myScore.textContent = playerScore;
+    //deciding the final winner.
+    if (computerScore == 5 || playerScore == 5) {
+        if (computerScore == 5) {
+            result.textContent = "-----Winner is Computer-----";
         }
+        else {
+            result.textContent = "-----You are the winner-----"
+        }
+        audio_winner.play();
+        playerScore = 0;
+        computerScore = 0;
     }
 }
+
+
 
 
